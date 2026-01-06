bf41ec8ac7aea7c32459421d69176b69490b130f
fatlum52 <fatlum_98@hotmail.com>
2026-01-06 14:44:15 +0100

    feat: implement swift data for trainingsession

--- repCounter/repCounter/model/TrainingSession.swift
+++ repCounter/repCounter/model/TrainingSession.swift
@@ -1,11 +1,19 @@
 import Foundation
 import SwiftData
 
+@Model
 final class TrainingSession: Identifiable {
-    let id           = UUID()
+    var id           = UUID()
     var date         = Date()
     var name: String = "Training vom "
+    @Relationship(deleteRule: .cascade)
     var exercises: [Exercise] = []
+    
+    init(name: String) {
+        self.name = name
+        self.date = Date()
+        self.exercises = []
+    }
     
     init(_ name: String, _ exercises: [Exercise]) {
         self.name = name
@@ -13,9 +21,13 @@ final class TrainingSession: Identifiable {
         self.date = Date()
     }
     
-    func getFormattedDate() -> String {
+    var formattedDate: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "E, d MMM y"
         return dateFormatter.string(from: date)
+    }
+    
+    func getFormattedDate() -> String {
+        return formattedDate
     }
 }
