95f3c0ce3be554094feef87359711cea0c44d6b7
fatlum52 <fatlum_98@hotmail.com>
2026-01-06 13:28:52 +0100

    feat: create Exercise object

--- repCounter/repCounter/model/Exercise.swift
+++ repCounter/repCounter/model/Exercise.swift
@@ -9,10 +9,31 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
-    var timestamp: Date
+final class Exercise: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+
+    // Reps, die über +/- gezählt werden
+    var quickReps: Int = 0
+
+    // „richtige“ Sets (als eingebettete Werttypen gespeichert)
+    var sets: [ExerciseSet] = []
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    init(_ name: String) {
+        self.name = name
+    }
+
+    struct ExerciseSet: Identifiable, Codable, Hashable {
+        var id: UUID = UUID()
+        var name: String
+        var reps: Int = 0
+        
+        init(_ name: String) {
+            self.name = name
+        }
+    }
+
+    var totalReps: Int {
+        quickReps + sets.reduce(0) { $0 + $1.reps }
     }
 }
