3bea7de956e80322a71c3e0f96fc00b34481d32b
fatlum52 <fatlum_98@hotmail.com>
2026-01-07 11:17:51 +0100

    feat: add text "Reps" to manual count

--- repCounter/repCounter/view/ExerciseDetailView.swift
+++ repCounter/repCounter/view/ExerciseDetailView.swift
@@ -24,6 +24,7 @@ struct ExerciseDetailView: View {
                         Text("\(exercise.quickReps)")
                             .font(.body)
                     }
+                    Text("Reps")
                 }
 
                 let rows = Array(exercise.sets.enumerated())
