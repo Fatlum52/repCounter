2ec75c12dbf8e47d54db8cfa7be45af608543b6d
fatlum52 <fatlum_98@hotmail.com>
2026-01-17 12:15:42 +0100

    feat: add notesarea in sheet view

--- repCounter/repCounter/view/components/NotesSheetView.swift
+++ repCounter/repCounter/view/components/NotesSheetView.swift
@@ -2,13 +2,35 @@ import SwiftUI
 
 struct NotesSheetView: View {
     
+    @Bindable var exercise: Exercise
+    
     var body: some View {
         
         Text("Hello World")
+        
+        VStack {
+            HStack {
+                Text("Notes")
+                    .font(.headline)
+                Spacer()
+                Text("Auto-Save")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            TextEditor(text: $exercise.notes)
+                .frame(minHeight: 100)
+                .padding(8)
+                .background(.regularMaterial)
+                .cornerRadius(16)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                )
+        }
         
     }
 }
 
 #Preview {
-    NotesSheetView()
+    //NotesSheetView()
 }
