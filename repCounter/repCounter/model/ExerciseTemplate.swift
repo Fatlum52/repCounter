9b4ec67e187706cfce4122003a0d24d2479bcc78
fatlum52 <fatlum_98@hotmail.com>
2026-01-19 00:32:10 +0100

    feat: create template for later library

--- repCounter/repCounter/model/ExerciseTemplate.swift
+++ repCounter/repCounter/model/ExerciseTemplate.swift
@@ -1,8 +1,12 @@
-//
-//  ExerciseTemplate.swift
-//  repCounter
-//
-//  Created by Fatlum Cikaqi on 18.01.2026.
-//
-
 import Foundation
+import SwiftData
+
+@Model
+final class ExerciseTemplate: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
