a41e293ca8ce538accfcdf706025680eaed1eb73
fatlum52 <fatlum_98@hotmail.com>
2026-01-18 15:57:37 +0100

    feat: add tabviews for main views

---
+++ repCounter/repCounter/view/MainTabView.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabView.swift
+//  repCounter
+//
+//  Created by Fatlum Cikaqi on 18.01.2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Tab("Workouts", systemImage: "figure.run") {
+                TrainingSessionView()
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
