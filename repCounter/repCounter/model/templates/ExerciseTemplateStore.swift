b077ed25c4fc180e24065aa8ca52ea1cdd006851
fatlum52 <fatlum_98@hotmail.com>
2026-01-20 17:15:18 +0100

    feat: implement store for choosing exercises

--- repCounter/repCounter/model/templates/ExerciseTemplateStore.swift
+++ repCounter/repCounter/model/templates/ExerciseTemplateStore.swift
@@ -1,8 +1,36 @@
-//
-//  ExerciseTemplateStore.swift
-//  repCounter
-//
-//  Created by Fatlum Cikaqi on 20.01.2026.
-//
-
 import Foundation
+
+final class ExerciseTemplateStore {
+
+    static let shared = ExerciseTemplateStore()
+
+    private(set) var templates: [ExerciseTemplate]
+
+    private init() {
+        self.templates = Self.defaultTemplates
+    }
+
+    func addTemplate(name: String) {
+        templates.append(ExerciseTemplate(name))
+    }
+
+    func removeTemplate(id: UUID) {
+        templates.removeAll { $0.id == id }
+    }
+
+    // MARK: - Defaults
+
+    private static let defaultTemplates: [ExerciseTemplate] = [
+        .init("Pushup"),
+        .init("Pullup"),
+        .init("Chinup"),
+        .init("Dip"),
+        .init("Muscle Up"),
+        .init("Bench Press"),
+        .init("Squat"),
+        .init("Deadlift"),
+        .init("Shoulder Press"),
+        .init("Pike Pushup"),
+        .init("Bicep Curl")
+    ]
+}
