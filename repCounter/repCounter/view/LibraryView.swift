fa908c1880ffff87e2339496423cdfcbe70dce1e
fatlum52 <fatlum_98@hotmail.com>
2026-01-20 22:59:22 +0100

    chore: deactivate sheets till it works

--- repCounter/repCounter/view/LibraryView.swift
+++ repCounter/repCounter/view/LibraryView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LibraryView: View {
     
     @State private var showExerciseSheet: Bool = false
+    @State private var showSessionSheet: Bool = false
     
     var body: some View {
         
@@ -19,26 +20,28 @@ struct LibraryView: View {
             }
             
             Button("Training Sessions") {
-                
+                showSessionSheet = true
             }
         }
         .buttonStyle(.borderedProminent)
         .tint(.orange)
         .font(.title3)
         .sheet(isPresented: $showExerciseSheet) {
-            ExerciseSheetTemplateView(
-                templates: [
-                    .init("Bench Press"),
-                    .init("Squat"),
-                    .init("Deadlift"),
-                    .init("Pull Ups"),
-                    .init("Shoulder Press"),
-                    .init("Pushup"),
-                    .init("Pullup")
-                ], onSelect: { _ in
-                    // nothing
-                }
-            )
+            // ExerciseSheetTemplateView
+            //TemplateSheetView(
+            //    templates: allTemplates,  // Defaults + User
+            //    title: "Exercise Templates",
+            //    onSelect: { name in addExercise(named: name) }
+            //)
+        }
+        
+        .sheet(isPresented: $showSessionSheet) {
+            // ExerciseSheetTemplateView
+            //TemplateSheetView(
+            //    templates: allTemplates,  // Defaults + User
+            //    title: "Session Templates",
+            //    onSelect: { name in addExercise(named: name) }
+            //)
         }
     }
 }
@@ -46,3 +49,6 @@ struct LibraryView: View {
 #Preview {
     LibraryView()
 }
+
+
+
