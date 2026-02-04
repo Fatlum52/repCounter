d21ca690ebfa1a13a3134fe5be23e974ebd81f48
fatlum52 <fatlum_98@hotmail.com>
2026-02-04 22:52:15 +0100

    feat: build the api client

--- repCounter/repCounter/utils/ExerciseAPIClient.swift
+++ repCounter/repCounter/utils/ExerciseAPIClient.swift
@@ -1,1 +1,27 @@
+import Foundation
 
+struct ExerciseAPIClient {
+    
+    private let baseURL = URL(string: "https://oss.exercisedb.dev")!
+    
+    func fetchExercises() async throws -> [ExerciseDTO] {
+        // crate url
+        let url = baseURL.appendingPathComponent("/api/v1/exercises")
+        
+        // fetch data
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let http = response as? HTTPURLResponse,
+              200..<300 ~= http.statusCode else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let decoded = try JSONDecoder().decode(ExerciseSearchResponseDTO.self, from: data)
+        
+        guard decoded.success else {
+            throw URLError(.cannotParseResponse)
+        }
+        
+        return decoded.data
+    }
+}
