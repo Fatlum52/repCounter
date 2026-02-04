1be7f640d501c7570f06ece21853f7c2caf4da8d
fatlum52 <fatlum_98@hotmail.com>
2026-02-04 23:05:15 +0100

    feat: fetch exercises changed to search for exercises

--- repCounter/repCounter/utils/ExerciseAPIClient.swift
+++ repCounter/repCounter/utils/ExerciseAPIClient.swift
@@ -4,9 +4,22 @@ struct ExerciseAPIClient {
     
     private let baseURL = URL(string: "https://oss.exercisedb.dev")!
     
-    func fetchExercises() async throws -> [ExerciseDTO] {
-        // crate url
-        let url = baseURL.appendingPathComponent("/api/v1/exercises")
+    func searchExercises(search: String, limit: Int = 10) async throws -> [ExerciseDTO] {
+        let trimmed = search.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !trimmed.isEmpty else { return [] }
+        
+        // create url
+        let endpoint = baseURL.appendingPathComponent("/api/v1/exercises")
+        var components = URLComponents(url: endpoint, resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = [
+            URLQueryItem(name: "search", value: trimmed),
+            URLQueryItem(name: "limit", value: String(limit))
+        ]
+        
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
         
         // fetch data
         let (data, response) = try await URLSession.shared.data(from: url)
