7dfa81db27696eae4b06e0c9da4a4adfb27fab0d
fatlum52 <fatlum_98@hotmail.com>
2026-02-09 15:51:38 +0100

    refactor: import newly setfield focus

--- repCounter/repCounter/Features/ExerciseDetail/ExerciseDetailView.swift
+++ repCounter/repCounter/Features/ExerciseDetail/ExerciseDetailView.swift
@@ -7,30 +7,6 @@ import AppKit
 import UniformTypeIdentifiers
 #endif
 
-#if os(iOS)
-struct Movie: Transferable {
-    let url: URL
-    
-    static var transferRepresentation: some TransferRepresentation {
-        FileRepresentation(contentType: .movie) { movie in
-            SentTransferredFile(movie.url)
-        } importing: { received in
-            let copy = URL.documentsDirectory.appending(path: "\(UUID().uuidString).mov")
-            if FileManager.default.fileExists(atPath: copy.path()) {
-                try FileManager.default.removeItem(at: copy)
-            }
-            try FileManager.default.copyItem(at: received.file, to: copy)
-            return Self.init(url: copy)
-        }
-    }
-}
-#endif
-
-enum SetFocusField: Hashable {
-    case weight(UUID)
-    case reps(UUID)
-}
-
 struct ExerciseDetailView: View {
     
     @Bindable var exercise: Exercise
