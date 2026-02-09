6795e6b7df61b4c4e9ee32f3667c86a626f9fa91
fatlum52 <fatlum_98@hotmail.com>
2026-02-09 15:52:26 +0100

    refactor: outsoure media types for video/foto import in separate file

---
+++ repCounter/repCounter/Services/MediaTransferTypes.swift
@@ -0,0 +1,22 @@
+#if os(iOS)
+import SwiftUI
+import UniformTypeIdentifiers
+
+/// Transferable wrapper for importing videos via PhotosPicker.
+struct Movie: Transferable {
+    let url: URL
+
+    static var transferRepresentation: some TransferRepresentation {
+        FileRepresentation(contentType: .movie) { movie in
+            SentTransferredFile(movie.url)
+        } importing: { received in
+            let copy = URL.documentsDirectory.appending(path: "\(UUID().uuidString).mov")
+            if FileManager.default.fileExists(atPath: copy.path()) {
+                try FileManager.default.removeItem(at: copy)
+            }
+            try FileManager.default.copyItem(at: received.file, to: copy)
+            return Self.init(url: copy)
+        }
+    }
+}
+#endif
